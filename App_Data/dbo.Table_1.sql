3f22e643f9f5f7ca13c57ecb3822d896a9424b14
ManamelaLKM <141572340+ManamelaLKM@users.noreply.github.com>
2023-09-01 18:26:13 +0200

    Add files via upload
    just updated database

---
+++ App_Data/dbo.Table_1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[FaqTable]
+(
+	[userId] INT NOT NULL , 
+    [Timestamp] TIME NULL, 
+    [Question] NVARCHAR(5000) NULL, 
+    [Answer] NVARCHAR(5000) NULL, 
+    [Intent] NVARCHAR(5000) NULL, 
+    [Confidence Score] INT NULL
+)
